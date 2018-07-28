98afed7de56a8129ae2f34364b9da74fae0f510e
Chambrse <shaneechambry@gmail.com>
2018-07-28 13:39:19 -0700

    added view counter

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(bodyParser.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
+
+let views = 0;
+
 var tables = [{
   customerName: "Shane",
   phoneNumber: "480-867-5309",
@@ -38,17 +41,25 @@ var waitListArray = [{
 
 
 // Basic route that sends the user first to the AJAX Page
+
+app.get("/api/views", function(req, res) {
+  res.send({views: views});
+});
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
+  views++;
 });
 
 app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
+  views++;
 });
 
 // Displays all characters
 app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
+  views++;
 });
 
 app.post("/api/tables", function(req, res) {
@@ -74,39 +85,6 @@ app.post("/api/clear/", function (req, res){
   tables = []
   waitListArray = []
 });
-
-
-// Displays a single character, or returns false
-/* app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
-}); */
-
-/* // Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body-parser middleware
-  var newcharacter = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newcharacter);
-
-  characters.push(newcharacter);
-
-  res.json(newcharacter);
-}); */
 
 // Starts the server to begin listening
 // =============================================================
